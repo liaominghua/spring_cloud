e059eb0375175c013d6e543bff5c7d98dbe62acf
liaominghua <312256159@qq.com>
2017-06-19 00:33:54 +0800

    yml配置和增加监控和管理

--- springboot/src/main/java/com/spring/cloud/springboot/HelloController.java
+++ springboot/src/main/java/com/spring/cloud/springboot/HelloController.java
@@ -1,7 +1,10 @@
 package com.spring.cloud.springboot;
 
+import org.springframework.boot.actuate.metrics.CounterService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * Created by minghua on 2017/6/18.
@@ -9,8 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    /**
+     * Http请求的性能指标之一，计数器，记录增加量和减少量
+     */
+    @Resource
+    private CounterService counterService;
+
     @RequestMapping("/hello")
     public String index(){
         return "Hello world";
+    }
+
+    @RequestMapping("/greet")
+    public String  greet(){
+        counterService.increment("hello.count");
+        return "";
     }
 }
